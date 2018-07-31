19dc0527c9152c92b61169379a1b11908c8b04dd
Ikhsanudin Hakim <ikhsanudinhakim@gmail.com>
2018-07-31 16:39:32 +0700

    first commit

---
+++ FancyButton/FancyButton.swift
@@ -0,0 +1,21 @@
+//
+//  FancyButton.swift
+//  FancyButton
+//
+//  Created by Simon Ng on 17/12/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class FancyButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
